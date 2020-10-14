870b153ac18ea7245bc858dfb6831199c5073cc1
Damien <damien111222@aol.com>
2020-10-14 03:34:31 -0700

    Added Message Deletion and Grammar Updates

--- better_antispam.js
+++ better_antispam.js
@@ -8,31 +8,31 @@ let messageLog = [];
 module.exports = async (client, options) => {
   /* Declaring our options which we are going to work on */
   
-  const limitUntilWarn = (options && options.limitUntilWarn) || 3; // Default value: 3. Explication: This is the limit where you get the warn message. If the member X sent over 3 messages within the interval, he get warned
-  const limitUntilMuted = (options && options.limitUntilMuted) || 5; // Default value: 5. Explication: This is the limit where you get Punished. If the member X sent over 5 messages within the interval, he get muted.
-  const interval = (options && options.interval) || 2000; //Default Time: 2000MS (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc...). Explication: The interval where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted
-  const warningMessage = (options && options.warningMessage) || "if you don't stop from spamming, I'm going to punish you!"; // Default Message: if you don't stop from spamming, I'm going to punish you!. Explication: None, it's just a message you get for the warning phase.
-  const muteMessage = (options && options.muteMessage) || "was muted since we don't like too much advertisement type people!"; // Default Message: "was muted since we don't like too much advertisement type people!". Explication: The message sent after member X was punished
-  const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7); // Default value: 7. Explication: When people are spamming the same message, <limitUntilWarn> is ignored and this will trigger when member X sent over 7+ message that are the same.
-  const maxDuplicatesMute = (options && options. maxDuplicatesMute || 10); // Deafult value: 10 Explication: The limit where member X get muted after sending too many messages(10+).
-  const ignoredRoles = (options && options.ignoredRoles) || []; // Default value: None. Explication: The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys.
-  const ignoredMembers = (options && options.ignoredMembers) || []; // Default value: None. Explication: These members are directly affected and they do not require to have the role above. Good for undercover pranks.
-  const mutedRole = (options && options.mutedRole) || "muted"; // Default value: muted. Explication: Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
-  const timeMuted = (options && options.timeMuted) || 1000 * 600; // Default value: 10 minutes. Explication: This is how much time member X will be muted. if not set, default would be 10 min.
-  const logChannel = (options && options.logChannel) || "AntiSpam-logs"; // Default value: "AhtiSpam-logs". Explication: This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
+  const limitUntilWarn = (options && options.limitUntilWarn) || 3; // Default value: 3. Explanation: This is the limit where you get the warn message. If the member X sent over 3 messages within the interval, he get warned
+  const limitUntilMuted = (options && options.limitUntilMuted) || 5; // Default value: 5. Explanation: This is the limit where you get Punished. If the member X sent over 5 messages within the interval, he get muted.
+  const interval = (options && options.interval) || 2000; //Default Time: 2000MS (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc...). Explanation: The interval where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted
+  const warningMessage = (options && options.warningMessage) || "If you don't stop spamming, I'm going to mute you!"; // Default Message: If you don't stop spamming, I'm going to mute you!. Explanation: None, it's just a message you get for the warning phase.
+  const muteMessage = (options && options.muteMessage) || "was muted due to spam."; // Default Message: "was muted due to spam.". Explanation: The message sent after member X was punished
+  const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7); // Default value: 7. Explanation: When people are spamming the same message, <limitUntilWarn> is ignored and this will trigger when member X sent over 7+ message that are the same.
+  const maxDuplicatesMute = (options && options. maxDuplicatesMute || 10); // Deafult value: 10 Explanation: The limit where member X get muted after sending too many messages(10+).
+  const ignoredRoles = (options && options.ignoredRoles) || []; // Default value: None. Explanation: The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys.
+  const ignoredMembers = (options && options.ignoredMembers) || []; // Default value: None. Explanation: These members are directly affected and they do not require to have the role above. Good for undercover pranks.
+  const mutedRole = (options && options.mutedRole) || "muted"; // Default value: muted. Explanation: Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
+  const timeMuted = (options && options.timeMuted) || 1000 * 600; // Default value: 10 minutes. Explanation: This is how much time member X will be muted. if not set, default would be 10 min.
+  const logChannel = (options && options.logChannel) || "AntiSpam-logs"; // Default value: "AhtiSpam-logs". Explanation: This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
 
 // If something is added wrong, throw an error
 
-  if(isNaN(limitUntilWarn)) throw new Error("ERROR: <limitUntilWarn> option is not set up right! Please check it again to be a number in settings.");
-  if(isNaN(limitUntilMuted)) throw new Error("ERROR: <limitUntilMuted> option is not set up right! Please add a number in settings.");
-  if(isNaN(interval)) throw new Error("ERROR: <interval> option is not set up right! Please add a number in settings.");
-  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("ERROR: <warningMessage> option must be a string and have at least 5 characters long (Including space).");
-  if(!isNaN(muteMessage) || muteMessage.length < 5) throw new Error("ERROR: <muteMessage> option must be a string and have at least 5 characters long (Including space).");
-  if(isNaN(maxDuplicatesWarning)) throw new Error("ERROR: <maxDuplicatesWarning> option is not set up right! Please check it again to be a number in settings.")
-  if(isNaN(maxDuplicatesMute)) throw new Error("ERROR: <maxDuplicatesMute> option is not set up right! Please check it again to be a number in settings.");
-  if(isNaN(timeMuted)) throw new Error("ERROR: <timeMuted> option is not set up right! Please check it again to be a number in settings.");
-  if(ignoredRoles.constructor !== Array) throw new Error("ERROR: <ignoredRoles> option is not set up right! Please check it again to be an array in settings.");
-  if(ignoredMembers.constructor !== Array) throw new Error("ERROR: <ignoredMembers> option is not set up right! Please check it again to be an array in settings.");
+  if(isNaN(limitUntilWarn)) throw new Error("ERROR: The <limitUntilWarn> option is not set up right! Please check it again to ensure it's a number in the settings.");
+  if(isNaN(limitUntilMuted)) throw new Error("ERROR: The <limitUntilMuted> option is not set up right! Please add a number in the settings.");
+  if(isNaN(interval)) throw new Error("ERROR: The <interval> option is not set up right! Please add a number in the settings.");
+  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("ERROR: The <warningMessage> option must be a string and at least 5 characters long (including space).");
+  if(!isNaN(muteMessage) || muteMessage.length < 5) throw new Error("ERROR: The <muteMessage> option must be a string and at least 5 characters long (including space).");
+  if(isNaN(maxDuplicatesWarning)) throw new Error("ERROR: The <maxDuplicatesWarning> option is not set up right! Please check it again to ensure it's a number in the settings.")
+  if(isNaN(maxDuplicatesMute)) throw new Error("ERROR: The <maxDuplicatesMute> option is not set up right! Please check it again to ensure it's a number in the settings.");
+  if(isNaN(timeMuted)) throw new Error("ERROR: The <timeMuted> option is not set up right! Please check it again to ensure it's a number in the settings.");
+  if(ignoredRoles.constructor !== Array) throw new Error("ERROR: The <ignoredRoles> option is not set up right! Please check it again to ensure it's an array in the settings.");
+  if(ignoredMembers.constructor !== Array) throw new Error("ERROR: The <ignoredMembers> option is not set up right! Please check it again to ensure it's an array in the settings.");
   
   // Custom 'checkMessage' event that handles messages
  client.on("checkMessage", async (message) => {
@@ -81,7 +81,7 @@ module.exports = async (client, options) => {
                 deny: ['VIEW_CHANNEL']
               }]
             })
-              .then(m=> m.send(`Created **\`anti-spam-Logs\`** channel since a channel for reports wasn't provided from beginning when setting up the module.`))
+              .then(m=> m.send(`Created a **\`anti-spam-Logs\`** channel since a channel for reports wasn't provided from the beginning when setting up the module.`))
               .catch(console.error)
   
         }catch(e){
@@ -109,7 +109,7 @@ module.exports = async (client, options) => {
                     SPEAK: false
                 });
             });
-           ReportChannel.send(`Created **\`muted\`** role since a role(to be applied to muted person) wasn't provided from beginning when setting up the module.`) 
+           ReportChannel.send(`Created a **\`muted\`** role since a role wasn't provided from the beginning when setting up the module.`) 
         } catch (e) {
             console.log(e.stack);
         }
@@ -117,13 +117,13 @@ module.exports = async (client, options) => {
     
       if (user) {
         user.roles.add(role).then(()=>{
-          m.channel.send(`<@!${m.author.id}>, ${muteMsg}`);
+          m.channel.send(`<@!${m.author.id}>, ${muteMsg}`).then(m=> m.delete(10000));
           let muteEmbed = new MessageEmbed()
             .setAuthor(' Action | Auto Mute', `https://images-ext-2.discordapp.net/external/Wms63jAyNOxNHtfUpS1EpRAQer2UT0nOsFaWlnDdR3M/https/image.flaticon.com/icons/png/128/148/148757.png`)
             .addField('Member muted:',`${user}`)
-            .addField(`How much time got muted?:`,`${timeMuted} seconds (10 min)`)
-            .addField('Reason of mute: ', `Spam`)
-            .addField(`When it was muted that person:`,TheDate+ " at "+ clock+" "+amORpm)
+            .addField(`Muted for:`,`${timeMuted} seconds (10 min)`)
+            .addField('Muted because: ', `Spam`)
+            .addField(`Muted At:`,TheDate+ " at "+ clock+" "+amORpm)
             .setColor('#D9D900')
           ReportChannel.send(muteEmbed);
           setTimeout(()=>{
@@ -137,7 +137,7 @@ module.exports = async (client, options) => {
           }, timeMuted);
           return true;
        }).catch((e) => {
-          m.guild.owner.send(`Oops, seems like i don't have sufficient permissions to mute <@!${message.author.id}>!\n It can be that or another type of error happened! Tell me on github: https://github.com/MirageZoe/ \n Everything happened on ${TheDate} at ${clock} ${amORpm} with message:\n\n\`${e.message}\`\n\n *P.S: If this is the first time getting something like this, most likely because it was not set up good the log channel at beginning and didn't know where to send the reports. Do not panic, next time it will work since he created the channel where to send the reports!*`);
+          m.guild.owner.send(`Oops, seems like I don't have sufficient permissions to mute <@!${message.author.id}>!\n It can be that or another type of error happened! Tell me on github: https://github.com/Damien111/better-discord-antispam \n Everything happened on ${TheDate} at ${clock} ${amORpm} with message:\n\n\`${e.message}\`\n\n *P.S: If this is the first time getting something like this, most likely it was because the log channel was not set up at the beginning and didn't know where to send the reports. Do not panic, next time it will work since we created the channel where to send the reports!*`);
           return false;
       });
     }//end of user
@@ -146,8 +146,9 @@ module.exports = async (client, options) => {
     
    // The warning function.
    const WarnMember = async (m, reply) => {
+    message.delete()
     warned.push(m.author.id);
-    m.channel.send(`<@${m.author.id}>, ${reply}`);
+    m.channel.send(`<@${m.author.id}>, ${reply}`).then(m=> m.delete(10000));
    }
 
     if (message.author.bot) return;
@@ -191,6 +192,7 @@ module.exports = async (client, options) => {
             WarnMember(message, warningMessage);
           } else if (matched == limitUntilMuted) {
             if (!punishedList.includes(message.author.id)) {
+              message.delete();
               MuteMember(message, muteMessage);
             }
           }
